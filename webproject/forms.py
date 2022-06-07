03c670032981efe1e0205fc075cc696389e08167
cheek12 <yongsa0221@gmail.com>
2022-06-07 18:32:06 +0900

    '0607-2'

--- webproject/forms.py
+++ webproject/forms.py
@@ -29,3 +29,4 @@ class UserCreateForm(FlaskForm):
 class UserLoginForm(FlaskForm):
     username=StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
     password=PasswordField('비밀번호', validators=[DataRequired()])
+
--- webproject/views/post_views.py
+++ webproject/views/post_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, Flask, redirect, g
+from flask import Blueprint, render_template, request, url_for, Flask, redirect, g, flash, session
 from werkzeug.utils import secure_filename
 from webproject.models import *
 from ..forms import *
@@ -10,13 +10,41 @@ from webproject.views.auth_views import login_required
 
 bp = Blueprint('post', __name__, url_prefix='/post')
 
-@bp.route('/list')
+@bp.route('/list/')
 def _list():
     page = request.args.get('page', type=int, default=1)
+    kw = request.args.get('kw', type=str, default='')
+    post_list = Post.query.order_by(Post.create_date.desc())
+    if kw:
+        search = '%%{}%%'.format(kw)
+        '''sub_query = db.session.query(Reply.post_id, Reply.content, User.username) \
+            .join(User, Reply.user_id == User.id).subquery()
+        post_list = post_list \
+            .join(User) \
+            .outerjoin(sub_query, sub_query.c.post_id == Post.id) \
+            .filter(Post.subject.ilike(search) |  # 질문제목
+                    Post.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
+            .distinct()'''
+        post_list = post_list.join(BookInfo).filter(
+            BookInfo.name.ilike(search)|
+            BookInfo.department.ilike(search)|
+            BookInfo.author.ilike(search)|
+            BookInfo.school.ilike(search)
+        )
+    post_list = post_list.paginate(page, per_page=10)
+    return render_template('post/post_list.html', post_list=post_list, page=page, kw=kw)
+
+
+
+'''    page = request.args.get('page', type=int, default=1)
     post_list = Post.query.order_by(Post.create_date.desc())
     post_list = post_list.paginate(page, per_page=10)
     return render_template('post/post_list.html', post_list=post_list)
-
+'''
 
 @bp.route('/detail/<int:post_id>')
 def detail(post_id):
@@ -65,3 +93,20 @@ def create():
 
     return render_template('post/post_form.html', form=form)
 
+@bp.route('/delete/<int:post_id>')
+@login_required
+def delete(post_id):
+    post=Post.query.get_or_404(post_id)
+    user_id = session.get('user_id')
+    post_list = Post.query.filter(Post.user_id == user_id).order_by(Post.create_date.desc())
+    reply_list = Reply.query.filter(Reply.user_id == user_id).order_by(Reply.create_date.desc())
+
+    if g.user != post.user:
+        flash('삭제권한이 없습니다')
+        return render_template('mypage/mypage_list.html', post_list=post_list, reply_list=reply_list)
+    db.session.query(Reply).filter(Reply.post_id==post_id).delete(synchronize_session=False)
+    db.session.delete(post)
+    db.session.commit()
+    post_list = Post.query.filter(Post.user_id == user_id).order_by(Post.create_date.desc())
+    reply_list = Reply.query.filter(Reply.user_id == user_id).order_by(Reply.create_date.desc())
+    return render_template('mypage/mypage_list.html', post_list=post_list, reply_list=reply_list)
